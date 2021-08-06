ead8e6a769bd068a97cfb532f7d4df328686aed9
Rafael <rflernesto@gmail.com>
2021-08-05 20:57:03 -0400

    test: Ensure RegisterUser throws when some data is missing

--- src/usecases/registerUser/register-user.spec.ts
+++ src/usecases/registerUser/register-user.spec.ts
@@ -2,6 +2,8 @@ import {RegisterUserUseCase} from "./register-user-use-case";
 import {InMemoryUserRepository} from "../../infra/data/in-memory/in-memory-user-repository";
 import {RegisterUser} from "./register-user";
 import {User} from "../../entities/user";
+import { UserResponseData } from "./helpers/user-response-data";
+import {InvalidUserData} from "../../entities/errors/invalid-user-data-error";
 
 describe('RegisterUser test', () => {
     const makeSut = (): RegisterUserUseCase => {
@@ -14,6 +16,18 @@ describe('RegisterUser test', () => {
             email:"any@mail.com",
             password: "12345678"
         })
-        expect(response).toBeInstanceOf(User)
+        expect(response.name).toEqual('Any_name')
+    })
+
+    it('Ensure RegisterUser throws when some data is missing', async () => {
+        const sut = makeSut()
+        try {
+            expect(await sut.create({
+                email:"any@mail.com",
+                password: "12345678"
+            })).toThrowError(InvalidUserData)
+        } catch(error) {
+            expect(error.message).toBe('Name must have length  between 1 and 150')
+        }
     })
 })
